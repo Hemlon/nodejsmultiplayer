dc1029f40710c6154694b42e8b0855ebc203f8c7
Hemlon <45729132+Hemlon@users.noreply.github.com>
2018-12-10 23:06:15 +1100

    updated to mobile friend version
    tap to move
    swipe to shoot

--- index.js
+++ index.js
@@ -1,24 +1,153 @@
+//dependencies
 var express = require('express');
-var http = require('http');
 var path = require('path');
+var http = require('http');
 var socketIO = require('socket.io');
+console.log("dependencies loaded");
 
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
+var portnum = process.env.PORT || 1337;
+console.log("packages created");
 
-app.set('port', 5000);
+app.set('port', portnum);
 app.use('/static', express.static(__dirname + '/static'));
+console.log("app directory static found");
 
 // Routing
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, 'index.html'));
 });
+console.log("routing file completed");
 
 // Starts the server.
-server.listen(5000, function() {
-  console.log('Starting server on port 5000');
+server.listen(portnum , function() {
+  console.log('Starting server on port%d', portnum);
 });
+
+
+var math = {
+    upper: function(value, limit) { 
+        if(value > limit) {
+            return limit;      
+        }
+        else {
+        return value;
+        }
+    },
+
+    lower: function(value, limit) {
+        if (value < limit) {
+            return limit;
+        }
+        else {
+            return value;
+        }
+    },
+
+    between: function (value, lowlimit, upplimit) {
+        value = this.upper(value, upplimit);
+        value = this.lower(value, lowlimit);
+        return value;
+    },
+
+    randInt: function (low, high) {return Math.round(low+Math.random()*   (high-low),0);},
+    toRad: function (angle) {return angle*Math.PI/180;}
+};
+
+var direction = function (from, x,y){
+	dy = y - from.y;
+	dx = x - from.x;
+	var angle = 0;
+	if(dx !== 0)
+	{
+	     angle = Math.atan(dy/dx)*180/Math.PI;
+	}
+	if (dx < 0)
+		angle = 180 + angle;
+	return angle;
+};
+
+var inBounds = function(object, xmin, xmax, ymin, ymax){
+    var inside = false;
+    if(object.x >= xmin && object.x <=xmax)
+    {
+        if(object.y >= ymin && object.y <= ymax)
+        {
+            inside = true;
+        }
+    }
+    return inside;
+};
+
+var motion = {
+        angle: 0,
+	freq: 0,
+	set: function(object, angle, speed)
+	{
+	  	object.x += speed *Math.cos(angle*Math.PI/180);
+		object.y -= speed *Math.sin(angle*Math.PI/180);
+	},
+
+	follow: function(follower, x,y, speed) {
+		var angle = -1*direction(follower, x, y);
+		if (!inBounds(follower, x-1,x+1,y-1,y+1))
+			this.set(follower,angle,speed);
+	},
+	
+	away: function(awayer, x,y,speed)
+	{
+		var angle = -1*direction(awayer, x, y) - 180;
+		this.set(awayer,angle,speed);
+	},
+
+	circular: function(object, speed, ang)
+	{	this.angle += ang;
+		if (this.angle >= 360) this.angle = 0;
+		this.set(object, this.angle, speed);
+	},
+
+	bop: function(object, amp, freq)
+	{		
+		this.freq += freq;
+		if (this.freq >=360) this.freq = 0;
+		object.y += amp*Math.sin(this.freq* Math.PI/180);
+	},
+        
+        move: function(object,damping)
+        {
+            var isMoving = false;
+            object.x += object.velocity.x;
+    		object.y -= object.velocity.y;
+    		object.velocity.x *= damping;
+    		object.velocity.y *= damping;
+            if (Math.abs(object.velocity.x) < 0.01 && Math.abs(object.velocity.y) < 0.01)
+                        isMoving = false;
+                else
+                        isMoving = true;
+                        
+                return isMoving;
+        }
+        
+};
+
+
+var projectile = function() {
+    this.x = 0;
+    this.y = 0;
+    this.angle = 0;
+    this.velocity = 3;
+    this.size = 5;
+    this.update = function() {
+        this.x += this.velocity*Math.cos(this.angle*Math.PI/180);
+        this.y += this.velocity*Math.sin(this.angle*Math.PI/180);
+    };
+};
+
+var projectiles = [];
+
+var mouse = {};
 
 setInterval(function() {
   io.sockets.emit('message', 'server is still running');
@@ -31,32 +160,82 @@ var playerSpd = 3;
 //register a new players when they connect
 io.on('connection', function(socket) {
   socket.on('new player', function() {
+	  var posx = math.randInt(0,320);
+	  var posy = math.randInt(0,480);
+	  var r=math.randInt(20,255);
+	  var g=math.randInt(20,255);
+	  var b=math.randInt(20,255);
     players[socket.id] = {
-      x: 300,
-      y: 300
+      x: posx,
+      y: posy,
+	  r: r,
+	  g: g,
+	  b: b
     };
   });
   
   //update new layer positions
-  socket.on('movement', function(data) {
+socket.on('keys', function(data) {
+	
     var player = players[socket.id] || {};
-    if (data[LEFT_ARROW]) {
-      player.x -= playerSpd;
-    }
-    if (data[UP_ARROW]) {
-      player.y -= playerSpd; 
-	  }
-    if (data[RIGHT_ARROW]) {
-      player.x += playerSpd;
-    }
-    if (data[DOWN_ARROW]) {
-      player.y += playerSpd 
-    }
-  });
-  
+	
+	//handles keyboard
+		if (data[37]) {
+		  player.x -= playerSpd;
+		}
+		
+		if (data[38]) {
+		  player.y -= playerSpd; 
+		}
+		  
+		if (data[39]) {
+		  player.x += playerSpd;
+		}
+	
+		if (data[40]) {
+			player.y += playerSpd 
+		}
+
+
 });
 
-//broadcast updated player state to all players
-setInterval(function() {
-  io.sockets.emit('state', players);
+socket.on('mouse', function(data) {
+	
+    var player = players[socket.id] || {};
+	mouse = data;
+	
+	if(data.isPressed && player.x != data.x && player.y != data.y && !data.isSwiped)
+	{
+		motion.follow(player,data.x, data.y, playerSpd);	
+	}
+	else
+	{
+		data.isPressed = false;
+	}
+	
+	if(data.isSwiped)
+	{	
+		p = new projectile();
+		p.x = player.x;
+		p.y = player.y;
+		p.angle = -1*direction(player, data.x, data.y);	
+		projectiles[projectiles.length] = p ;
+		//console.log(p.angle);
+	}		
+});
+
+});
+
+//update and broadcast state to all players
+setInterval(function() {	
+	for(var i = 0; i < projectiles.length; i++) {
+		
+		motion.set(projectiles[i], projectiles[i].angle, 5);
+		//projectilei[i].x += 1;
+	}	
+	
+  io.sockets.emit('projectiles', projectiles);
+   io.sockets.emit('state', players);
 }, 1000 / 60);
+
+
--- static/game.js
+++ static/game.js
@@ -1,72 +1,104 @@
-var movement = {
-  up: false,
-  down: false,
-  left: false,
-  right: false
-}
-
-
 var socket = io();
 socket.on('message', function(data) {
-  console.log(data);
+	console.log(data);
 });
 
-//tell server new player has arrived
+var movement = {
+	up: false,
+	down: false,
+	left: false,
+	right: false
+}
+var players = {};
+var projectiles = {};
+var keys = {};
+
+var mouseDat = {
+	x: 0,
+	y: 0,
+	isPressed: false,
+	isSwiped: false
+}
+
+var element = document.getElementsByTagName('BODY')[0];
+
+Hammer(element).on("swipe", function () {	
+	mouseDat.isSwiped = true;
+});
+
+Hammer(element).on("tap", function () {	
+	mouseDat.isPressed = true;
+});
+
+
+var keyPressed = function() 
+{
+	keys[keyCode] = true;
+}
+
+var keyReleased = function()
+{
+	keys[keyCode] = false;
+}
+
 socket.emit('new player');
 
-
-document.addEventListener('keydown', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      movement.left = true;
-      break;
-    case 87: // W
-      movement.up = true;
-      break;
-    case 68: // D
-      movement.right = true;
-      break;
-    case 83: // S
-      movement.down = true;
-      break;
-  }
-});
-
-
-document.addEventListener('keyup', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      movement.left = false;
-      break;
-    case 87: // W
-      movement.up = false;
-      break;
-    case 68: // D
-      movement.right = false;
-      break;
-    case 83: // S
-      movement.down = false;
-      break;
-  }
-});
-
-//set up canvas
-var canvas = document.getElementById('canvas');
-canvas.width = 800;
-canvas.height = 600;
-var context = canvas.getContext('2d');
-socket.on('state', function(players) {
-  context.clearRect(0, 0, 800, 600);
-  context.fillStyle = 'blue';
-  for (var id in players) {
-    var player = players[id];
-    context.beginPath();
-    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
-    context.fill();
-  }
-});
-
-//update movements to server
 setInterval(function() {
-  socket.emit('movement', movement);
+	socket.emit('keys', keys);
 }, 1000 / 60);
+
+setInterval(function() {
+	mouseDat.x = mouseX;
+	mouseDat.y = mouseY;
+	if (mouseDat.isSwiped)
+	{
+		mouseDat.isPressed = false;
+	}
+	socket.emit('mouse', mouseDat);	
+	//clear data
+	mouseDat.isSwiped = false;
+	
+}
+, 1000/60);
+
+this.setup = function() {
+	canvas = createCanvas(POP.WIDTH,POP.HEIGHT);
+};
+
+this.draw = function(){	
+
+	background(50,50,50);	
+	noStroke();		
+	for (var id in players) {
+		player = players[id];
+		fill(player.r,player.g,player.b);
+		rect(player.x, player.y, 10,10);
+	}	
+	
+	for(var id in projectiles){
+		projectile = projectiles[id];
+		fill(player.r, player.g, player.b);
+		ellipse(projectile.x, projectile.y, projectile.size, projectile.size);
+	}
+	
+	textSize(15);
+	fill(255,255,255);
+	text("Mr Hem's Multiplayer Node.JS test", 40,20);
+	text(mouseDat.x + "," + mouseDat.y + "," + mouseDat.isPressed, 100,100);
+};
+
+//received updated client states
+socket.on('state', function(clients) {
+		players = clients;
+});
+
+socket.on('projectiles', function(data) {
+	   projectiles = data;
+});
+
+
+
+
+	
+	
+
